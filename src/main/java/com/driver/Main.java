b423119599d9dcf3509858c1d48527bf4f0f7e42
waqar <waqarh10@gmail.com>
2023-10-08 01:37:37 +0530

    done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,16 @@
 package com.driver;
 
 public class Main {
+    public static void main(String args[]){
+        RWOnly obj = new RWOnly();
+
+        obj.setName("Tushar");
+
+        String ans =obj.getName();
+
+        System.out.println(ans);
+
+
+    }
   
 }
